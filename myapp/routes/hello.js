1966fe8b9f2c603be98294a8b82740d4f9881052
sakuraipciphone <sakurai.pc.iphone@spice.ocn.ne.jp>
2026-05-10 15:43:29 +0900

    add hello page

---
+++ myapp/routes/hello.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('hello', { title: 'Hello Express' });
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
